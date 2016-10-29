39a66c388609c4ef54172cc1dd63729df17806b5
phuong <taiphuongit@gmail.com>
2016-10-29 16:50:59 +0700

    them ecom demo

---
+++ ecom_demo/__openerp__.py
@@ -0,0 +1,18 @@
+{
+    'name': 'Ecommerce Demo',
+    'description': 'Ecommerce Demo',
+    'summary': 'Website.',
+    'category': 'WEBSITE',
+    'version': '1.0',
+    'website': 'http://www.besco.vn',
+    'author': 'TaiPhuong',
+    'depends': ['website','website_blog', 'website_crm','website_sale'],
+    'data': ['view/layout.xml','view/snippets.xml','view/options.xml','view/asset.xml'],
+    'css': ['static/src/css/*.css'],
+    'js' : ['static/src/js/*.js'],
+    'images':[
+    ],
+    'installable': True,
+    'auto_install': False,
+    'certificate': False,
+}
